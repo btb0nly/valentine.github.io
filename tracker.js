38dee2aec644391701223ed55785c12a551925d0
btb0nly <briantbayaca@gmail.com>
2026-02-07 11:39:02 +0100

    Refactor logChoice to use URLSearchParams
    Updated the logChoice function to use URLSearchParams for the request body instead of JSON.stringify.

--- tracker.js
+++ tracker.js
@@ -1,14 +1,14 @@
 const ENDPOINT = "https://script.google.com/macros/s/AKfycbxL2oIc02-q8lNmwTAwHQ-EFsxkWLNHN3vl4vXqqJbitSURJdpgOtrHs1yOGPi5483y/exec";
 
 function logChoice(page, choice, extra = "") {
+  const body = new URLSearchParams();
+  body.append("page", page);
+  body.append("choice", choice);
+  body.append("extra", extra);
+  body.append("userAgent", navigator.userAgent);
+
   fetch(ENDPOINT, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      page,
-      choice,
-      extra,
-      userAgent: navigator.userAgent
-    })
+    body
   }).catch(() => {});
 }
