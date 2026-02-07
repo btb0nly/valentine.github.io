ee26ad41fcbda2fe825685f0de5511ba45727187
btb0nly <briantbayaca@gmail.com>
2026-02-07 11:34:57 +0100

    Update API endpoint URL in tracker.js

--- tracker.js
+++ tracker.js
@@ -1,4 +1,4 @@
-const ENDPOINT = "https://script.google.com/macros/s/AKfycbymrXhG-dPCV7DmakZE2LglGE4Pi_M1jDOM-SbKcYLi7zsRvVR_6C8M8Q5fTvqdPuJa/exec";
+const ENDPOINT = "https://script.google.com/macros/s/AKfycbxL2oIc02-q8lNmwTAwHQ-EFsxkWLNHN3vl4vXqqJbitSURJdpgOtrHs1yOGPi5483y/exec";
 
 function logChoice(page, choice, extra = "") {
   fetch(ENDPOINT, {
